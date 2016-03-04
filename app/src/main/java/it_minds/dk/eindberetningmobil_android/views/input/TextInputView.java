b790a276f70c36716fc4805503454e259d7b10c7
Marc le Fevre Johansen <mfj@it-minds.dk>
2016-03-04 11:04:26 +0100

    Aligned purpose input with iPhone functionality done button

--- app/src/main/java/it_minds/dk/eindberetningmobil_android/views/input/TextInputView.java
+++ app/src/main/java/it_minds/dk/eindberetningmobil_android/views/input/TextInputView.java
@@ -3,7 +3,9 @@ package it_minds.dk.eindberetningmobil_android.views.input;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import it_minds.dk.eindberetningmobil_android.R;
 import it_minds.dk.eindberetningmobil_android.baseClasses.ProvidedSimpleActivity;
@@ -22,6 +24,16 @@ public class TextInputView extends ProvidedSimpleActivity {
 
         setContentView(R.layout.simple_text_input_view);
         field = getViewById(R.id.simple_text_input_view_field);
+        field.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(event != null && event.getKeyCode() == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
+                    onBackPressed();
+                    return true;
+                }
+                return false;
+            }
+        });
         String providedText = getIntent().getStringExtra(IntentIndexes.DATA_INDEX);
         if (providedText != null) {
             field.setText(providedText);
@@ -30,7 +42,6 @@ public class TextInputView extends ProvidedSimpleActivity {
         if (title == null) {
             title = "";
         }
-
         setActionbarBackDisplay(title);
 
     }
@@ -43,4 +54,6 @@ public class TextInputView extends ProvidedSimpleActivity {
         setResult(Activity.RESULT_OK, resultData);
         super.onBackPressed();
     }
+
+
 }
